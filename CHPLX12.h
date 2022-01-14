c2676f00000f4f8a78602dab38052e68990d0297
István Szénási <szeist@gmail.com>
2022-01-14 01:04:24 +0100

    fix number display (header)

--- CHPLX12.h
+++ CHPLX12.h
@@ -2,7 +2,7 @@
 #define _DRIVER_CHPLX12
 
 class CHPLX12 {
-
+  
   public:
 
   CHPLX12(uint8_t pin1, uint8_t pin2, uint8_t pin3, uint8_t pin4);
@@ -14,10 +14,25 @@ class CHPLX12 {
   private:
 
   void setPinPair(uint8_t high, uint8_t low) const;
+  void clearPinPair(uint8_t high, uint8_t low) const;
 
   uint8_t pins[4];
 
-  static const uint8_t trackPairs[12][2];
+  // {high, low, clear1, clear2}
+  const uint8_t trackPairs[12][4] = {
+    {1, 0, 2, 3},
+    {0, 1, 2, 3},
+    {2, 1, 0, 3},
+    {1, 2, 0, 3},
+    {3, 2, 0, 1},
+    {2, 3, 0, 1},
+    {2, 0, 1, 3},
+    {0, 2, 1, 3},
+    {3, 1, 0, 2},
+    {1, 3, 0, 2},
+    {3, 0, 1, 2},
+    {0, 3, 1, 2}
+  };
 };
 
 #endif
